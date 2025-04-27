34bc4e07a1517496927f373c8ab4d9923189d42b
jacksonaustin <jacksonaustin@jacksons-air-2.attlocal.net>
2025-04-26 19:32:32 -0500

    Added tiles

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -1,22 +1,44 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame{
 	
 	private GameScreen gameScreen; 
 	
+	private BufferedImage img; 
+	
+	
 	public Game() {
-		setSize(1024, 640); 
+		
+		this.importing(); 
+		
+		setSize(800,800); 
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 
 		setLocationRelativeTo(null); 
 		
-		gameScreen = new GameScreen(); 
+		gameScreen = new GameScreen(img); 
 		add(gameScreen); 
 		setVisible(true);
 
 	}
 	
+	private void importing() {
+		InputStream is = getClass().getResourceAsStream("downscaled_32x32.png");
+		
+		try {
+			img = ImageIO.read(is); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		
+		}
+	
+	}
 	public static void main(String[] args) {
 		
 		
--- src/main/java/main/GameScreen.java
+++ src/main/java/main/GameScreen.java
@@ -3,13 +3,18 @@ package main;
 import java.awt.Color;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel {
 	
-	public GameScreen() {
+	private BufferedImage img; 
+	
+	public GameScreen(BufferedImage img) {
+		
+		this.img = img; 
 		
 		
 	}
@@ -17,23 +22,31 @@ public class GameScreen extends JPanel {
 	public void paintComponent(Graphics g) {
 
 		super.paintComponent(g);
-		int p = 0;
+		
+		g.drawImage(img,0, 0, null);
+		
+		int x = 0;
+		int y = 0;
 		
 		
-		for(int r = 0; r < 32; r ++) {
+	for(int r = 0; r < 32; r ++) {
+
 			for(int c = 0; c < 32; c ++) {
 
-			
-				g.setColor(randColor());
-				g.fillRect(32 * c, p,  32, 32);
+					g.drawImage(img, x , y , null);
+					
+				x += 32;
 				
 			}
 			
-			p+=32; 
-			
+			y += 32; 
+			x = 0;
+						
 		}
 		
 	}
+
+
 	
 	public static Color randColor() {
 		
