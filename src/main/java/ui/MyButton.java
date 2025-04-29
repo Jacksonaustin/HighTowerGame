ba90fcdd289c0c91636bc0e162156a0375e00a6e
karol <karol@Karol>
2025-04-28 21:47:29 -0500

    MyButton

---
+++ src/main/java/ui/MyButton.java
@@ -0,0 +1,45 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class MyButton {
+
+	private int x, y, width, height;
+	private String text;
+	private Rectangle bounds;
+	
+	public MyButton(String text, int x, int y, int width, int height) {
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
+		initBounds();
+	}
+	
+	private void initBounds() {
+		this.bounds = new Rectangle(x, y, width, height);
+	}
+	
+	public void draw(Graphics g) {
+		
+		//Body
+		g.setColor(Color.WHITE);
+		g.fillRect(x, y, width, height);
+		
+		//Border
+		g.setColor(Color.black);
+		g.drawRect(x, y, width, height);
+		
+		//Text
+		g.drawString(text, x, y);
+		
+	}
+	
+	public Rectangle getBounds() {
+		return bounds;
+	}
+}
